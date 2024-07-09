37d28a8cb5db267a62081c91663431efc290f071
htdhcvm <flouheforst@gmail.com>
2024-07-09 21:52:55 +0300

    stage-4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,10 +23,20 @@ public class Main {
             String line = in.readLine();
 
             String[] partsLine = line.split(" ");
+            System.out.println(Arrays.toString(partsLine));
+
             String path = partsLine[1];
 
-            if(path.equals("/")) {
+            String[] split = path.substring(1).split("/");
+
+            if (path.equals("/")) {
                 clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            }
+            else if (split[0].equals("echo") && split.length == 2) {
+                String param = split[1];
+                String response = String.format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", param.length(), param);
+                System.out.println(response);
+                clientSocket.getOutputStream().write(response.getBytes());
             } else {
                 clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
             }
